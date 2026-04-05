43e43c3efac8a17dd2e8fecc9174b69de1fe6a28
103ftt-cmd <103ftt@gmail.com>
2026-04-05 07:19:05 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfilePage from './Profile';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProfilePage />
+    </div>
+  );
+};
+
+export default App;
